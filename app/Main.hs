21bb67f62b2b4b238e3de110c6686954d5ba26d8
Shane Kennedy <shane.kennedy19@gmail.com>
2019-08-25 21:15:27 +0200

    Refactor to Nim module

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,6 @@
 module Main where
 
-whosTurn :: Bool -> String
-whosTurn True = "User's turn!"
-whosTurn _ = "Computer's turn!"
-
-getHumanInput :: Int -> IO String
-getHumanInput count = do
-  putStrLn $
-    "Enter a number between 1 and " ++ show (ceiling $ fromIntegral count / 2)
-  num <- getLine
-  return num
-
-reportScore :: Int -> IO ()
-reportScore num = putStrLn $ "The count is now " ++ show num
-
-playHumanTurn :: Int -> IO ()
-playHumanTurn total = do
-  putStrLn $ whosTurn True
-  num <- getHumanInput total
-  reportScore (total - (read num))
-  playNim (total - (read num)) False
-
-playComputerTurn :: Int -> IO ()
-playComputerTurn total = do
-  putStrLn $ whosTurn False
-  reportScore (total - 1)
-  playNim (total - 1) True 
+import Nim
   
-
-playNim :: Int -> Bool -> IO ()
-playNim total userTurn
-  | total <= 0 = putStrLn (whosTurn userTurn ++ " wins!")
-  | userTurn = playHumanTurn total 
-  | otherwise = playComputerTurn total
-   
 main :: IO ()
 main = playNim 50 True
